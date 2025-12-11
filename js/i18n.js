47b2ab2f660dc7b2ea935e39f26c16297391dfdd
juancatoira <juan250595@gmail.com>
2025-12-11 16:56:50 +0100

    update languaje change

--- js/i18n.js
+++ js/i18n.js
@@ -1,6 +1,14 @@
 const defaultLanguage = localStorage.getItem("lang") || "es";
 
 async function loadLanguage(lang) {
+  const body = document.getElementById("body");
+
+  // Fade out
+  body.style.opacity = 0;
+
+  // Espera un momento para que el fade out sea visible
+  await new Promise(resolve => setTimeout(resolve, 150));
+
   const response = await fetch(`lang/${lang}.json`);
   const translations = await response.json();
 
@@ -21,6 +29,11 @@ async function loadLanguage(lang) {
 
   localStorage.setItem("lang", lang);
   document.documentElement.lang = lang;
+
+  // Fade in
+  requestAnimationFrame(() => {
+    body.style.opacity = 1;
+  });
 }
 
 function setLanguage(lang) {
