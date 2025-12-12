75a6f3db813456a89c330f733a6cf92ca379e819
juancatoira <juan250595@gmail.com>
2025-12-12 13:33:38 +0100

    js fix

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const nameSection = document.querySelector('h1');
 // Verificamos la posición al hacer scroll
 window.addEventListener('scroll', function() {
   // Verificamos si hemos pasado el nombre (h1) al hacer scroll
+  const scrollPosition = window.scrollY;
+
+  // Si la posición del scroll es mayor que la distancia desde el top del título
+  if (scrollPosition > nameSection.offsetTop) {
+    // Añadimos la clase 'visible' para mostrar el header
+    header.classList.add('visible');
+  } else {
+    // Si no, eliminamos la clase 'visible' para ocultarlo
+    header.classList.remove('visible');
+  }
+});
